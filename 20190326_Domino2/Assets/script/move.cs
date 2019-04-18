1e98d379e8a9f637b1ae13c62eb384b768cdc98e
SagacityJang <gkagm2@gmail.com>
2019-04-18 12:23:35 +0900

    도미노 만들기

---
+++ 20190326_Domino2/Assets/script/move.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(0, 0, -40 * Time.deltaTime);
+        Debug.Log(Time.deltaTime);
+    }
+}
