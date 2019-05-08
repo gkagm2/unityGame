2f30ef5782a38880a4b92ae0641ae0bbc79963b6
HyeonMyeongJang <gkagm2@gmail.com>
2019-05-08 23:34:59 +0900

    PlayerInfo 변경

---
+++ 20190502_FillOneLinePuzzle/Scripts/Game/PlayerInfo.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInfo : MonoBehaviour {
+
+    [Header("Player")]
+    [Tooltip("현재 가지고 있는 코인")]
+    public int coin;
+
+    [Space]
+
+    [Tooltip("얻을 수 있는 최대 별의 개수")]
+    public int maxStar;
+
+    [Space]
+
+    [Tooltip("현재 가지고있는 별의 총 개수")]
+    public int currentTotalStar;
+
+    [Space]
+
+    [Tooltip("레벨마다의 최대 별의 개수")]
+    public int levelMaxStar;
+
+    [Space]
+
+    [Tooltip("각 레벨에 해당하는 별의 개수")]
+    public int currentLevelTotalStar;
+
+    
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
