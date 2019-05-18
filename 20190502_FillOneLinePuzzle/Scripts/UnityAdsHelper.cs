5678c01752766f8a7c5d2e981d23a7a130d0b7b9
HyeonMyeongJang <gkagm2@gmail.com>
2019-05-18 18:17:47 +0900

    임시 광고기능 넣어보기 (테스트 성공)

---
+++ 20190502_FillOneLinePuzzle/Scripts/UnityAdsHelper.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements;
+public class UnityAdsHelper : MonoBehaviour {
+    const string androidGameID = "xxxxxxxx";
+    const string IosGameID = "xxxxxxxx";
+
+    const string rewardedVideoID = "rewardedVideo";
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Advertisement.Show(); // 광고 호출.
+	}
+    private void Initialize()
+    {
+    }
+    public void ShowRewardedAd()
+    {
+        if (Advertisement.IsReady("rewardedVideo"))
+        {
+            var options = new ShowOptions { resultCallback = HandleShowResult };
+            Advertisement.Show("rewardedVideo", options);
+        }
+    }
+
+    private void HandleShowResult(ShowResult result)
+    {
+        switch (result)
+        {
+            case ShowResult.Finished:
+                Debug.Log("The as was successfully shown.");
+                break;
+            case ShowResult.Skipped:
+                Debug.Log("The ad was skipped before reaching the end.");
+                break;
+            case ShowResult.Failed:
+                Debug.LogError("The ad failed to be shown");
+                break;
+
+        }
+    }
+}
+
