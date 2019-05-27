473c89833d9d2d2d2a43ef0b4f867c4c17fbdcfb
SagacityJang <gkagm2@gmail.com>
2019-05-27 17:41:10 +0900

    update

---
+++ 20190524_RPG/Scripts/Cube/Test.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+    Animator anim;
+    Rigidbody rigid;
+    BoxCollider col;
+
+    [SerializeField] float moveSpeed;
+    [SerializeField] float jumpForce;
+    [SerializeField] LayerMask layerMask;
+
+    bool isMove;
+    bool isJump;
+    bool isFall;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+        rigid = GetComponent<Rigidbody>();
+        col = GetComponent<BoxCollider>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        TryWalk();
+        TryJump();
+
+	}
+
+    private void TryWalk()
+    {
+        float _dirX = Input.GetAxisRaw("Horizontal"); // 좌측 -1 우측 1 안누르면 0 리턴
+        float _dirZ = Input.GetAxisRaw("Vertical"); // 위 1 아래 -1 안누르면 0 리턴
+
+
+        Vector3 direction = new Vector3(_dirX, 0, _dirZ);
+        isMove = false;
+        if (direction != Vector3.zero)
+        {
+            isMove = true;
+            // 대각선 움직임도 수평 수직 움직임과 똑같이 만들기 위해서 normalized 함. 하지 않으면 대각선 속도만 유독 빨라짐.
+            transform.Translate(direction * moveSpeed * Time.deltaTime);
+        }
+
+        anim.SetBool("Move", isMove);
+        anim.SetFloat("DirX", direction.x);
+        anim.SetFloat("DirZ", direction.z);
+    }
+
+    private void TryJump()
+    {
+
+        if (isJump)
+        {
+            if(rigid.velocity.y <= 0.1f && !isFall)
+            {
+                isFall = true;
+                anim.SetTrigger("Fall");
+            }
+            RaycastHit hitInfo;
+            if (Physics.Raycast(transform.position,-transform.up, out hitInfo, col.bounds.extents.y + 0.05f, layerMask)){
+                anim.SetTrigger("Land");
+                isJump = false;
+                isFall = false;
+            }
+        }   
+
+        if(Input.GetKeyDown(KeyCode.Space) && !isJump)
+        {
+            isJump = true;
+            //rigid.velocity = new Vector3(0, jumpForce, 0);
+            rigid.AddForce(Vector3.up * jumpForce);
+            anim.SetTrigger("Jump");
+        }
+    }
+}
---
+++ 20190524_RPG/Scripts/Player/PlayerMove.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour {
+    Animator anim;
+    CharacterController character;
+
+    [SerializeField] float moveSpeed;
+
+    bool isMove;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        TryWalk();
+
+    }
+
+    private void TryWalk()
+    {
+        float _dirX = Input.GetAxisRaw("Horizontal"); //좌, 우
+        float _dirZ = Input.GetAxisRaw("Vertical"); // 위, 아래
+
+        Vector3 direction = new Vector3(_dirX, 0, _dirZ);
+
+        isMove = false;
+        if (direction != Vector3.zero)
+        {
+            isMove = true;
+            transform.Translate(direction * moveSpeed * Time.deltaTime);
+        }
+
+        anim.SetBool("Move", isMove);
+        anim.SetFloat("DirX", direction.x);
+        anim.SetFloat("DirZ", direction.z);
+    }
+}
