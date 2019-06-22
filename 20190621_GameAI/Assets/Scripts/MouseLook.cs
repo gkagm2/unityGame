f139b4bcd0015d0975daeed2b29125bf693406df
HyeonMyeongJang <gkagm2@gmail.com>
2019-06-22 14:56:41 +0900

    mouse 움직일 때 카메라 돌아가기 구현

---
+++ 20190621_GameAI/Assets/Scripts/MouseLook.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour {
+
+    public float sensitivity = 700.0f;
+    public float rotationX;
+    public float rotationY;
+
+	void Update () {
+        float mouseMoveValueX = Input.GetAxis("Mouse X");
+
+        float mouseMoveValueY = Input.GetAxis("Mouse Y");
+
+        rotationY += mouseMoveValueX * sensitivity * Time.deltaTime;
+        rotationX += mouseMoveValueY * sensitivity * Time.deltaTime;
+
+        // 상하
+        if(rotationX > 90f)
+            rotationX = 90f;
+        if (rotationX < -90f)
+            rotationX = -90f;
+
+        transform.eulerAngles = new Vector3(-rotationX, rotationY, 0.0f);
+        print(transform.eulerAngles.x + " , " + transform.eulerAngles.y + ", " + transform.eulerAngles.z);
+	}
+}
