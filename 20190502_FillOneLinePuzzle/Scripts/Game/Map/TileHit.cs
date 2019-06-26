9457708b86d64f79f841fdb1a23c001785b01076
HyeonMyeongJang <gkagm2@gmail.com>
2019-06-27 01:07:53 +0900

    타일을 누른 상태에서 드래그할 수 있게 바꿈

--- 20190502_FillOneLinePuzzle/Scripts/Game/Map/TileHit.cs
+++ 20190502_FillOneLinePuzzle/Scripts/Game/Map/TileHit.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public class TileHit : MonoBehaviour {
     public Camera camera;
-    
 
-    public 
+
+    public bool isClick = false;
         
 	// Use this for initialization
 	void Start () {
@@ -18,47 +18,60 @@ public class TileHit : MonoBehaviour {
 
         // TODO : 마우스를 누르고 있으면 깜빡거리는 현상이 일어난다. 
         // 이를 해결하기 위해 임시로 Input.GetKeyDown으로 설정해 놓음. 고쳐야 함.
-        if (Input.GetKeyDown(KeyCode.Mouse0)) 
+        if (Input.GetKey(KeyCode.Mouse0)) 
         {
+            isClick = true;
             //GameObject map = GameObject.FindGameObjectWithTag("Map"); //Map이라는 Tag를 가진 GameObject를 가져온다.
             //Debug.Log("map name : " + map.name);
+        }
+        if (Input.GetKeyUp(KeyCode.Mouse0))
+        {
+            isClick = false;
+        }
+        if (isClick)
+        {
+            ScreenRayHit();
+        }
 
-            Ray ray = camera.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
-            if (Physics.Raycast(ray, out hit))
+    }
+
+    public void ScreenRayHit()
+    {
+        Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (hit.collider.tag == "Tile") //맞은 tile이면
             {
-                if (hit.collider.tag == "Tile") //맞은 tile이면
+                GameObject tileObj = hit.collider.gameObject as GameObject; // tile의 Object를 obj로 옮김
+                UISprite tileSprite = tileObj.GetComponent<UISprite>(); // UISprate 컴포넌트를 가져옴.
+
+                Tile tile = tileObj.GetComponent<Tile>(); //타일 컴포넌트를 가져옴.
+
+                TileControl tileControl = tileObj.transform.parent.GetComponent<TileControl>(); // 맞은 타일 오브젝트의 tileControl을 가져옴.
+
+                if (tileControl.tileStack.Contains(tile)) // 누른 타일이 이미 stack에 존재하는 타일이면
                 {
-                    GameObject tileObj = hit.collider.gameObject as GameObject; // tile의 Object를 obj로 옮김
-                    UISprite tileSprite = tileObj.GetComponent<UISprite>(); // UISprate 컴포넌트를 가져옴.
-                    
-                    Tile tile = tileObj.GetComponent<Tile>(); //타일 컴포넌트를 가져옴.
-
-                    TileControl tileControl = tileObj.transform.parent.GetComponent<TileControl>(); // 맞은 타일 오브젝트의 tileControl을 가져옴.
-
-                    if (tileControl.tileStack.Contains(tile)) // 누른 타일이 이미 stack에 존재하는 타일이면
-                    {
-                        // 눌렀던 타일 이후에 stack에 push된 타일은 stack에서 pop한다.
-                        while (tileControl.tileStack.Peek() != tile) // 타일이 누른 타일과 같지 않다면
-                        {//같을때까지 pop하라
-                            Tile tempT = tileControl.tileStack.Pop();
-                            tempT.ChangeColor();
-                            //Debug.Log("pop한 타일 : " + tempT.name);
-                        }
+                    // 눌렀던 타일 이후에 stack에 push된 타일은 stack에서 pop한다.
+                    while (tileControl.tileStack.Peek() != tile) // 타일이 누른 타일과 같지 않다면
+                    {//같을때까지 pop하라
+                        Tile tempT = tileControl.tileStack.Pop();
+                        tempT.ChangeColor();
+                        //Debug.Log("pop한 타일 : " + tempT.name);
                     }
+                }
 
-                    // 누른 타일이 현재 스택 맨 위에 있는 타일과 충돌하는 타일이면
-                    if (tileControl.tileStack.Peek().collisionTileStack.Contains(tile))
-                    {
-                        tile.ChangeColor(); //색을 바꾼다.
-                    }
-                    else
-                    {
-                        Debug.Log("멀리 떨어져있는 타일이여서 색 못바꿈");
-                    }
+                // 누른 타일이 현재 스택 맨 위에 있는 타일과 충돌하는 타일이면
+                if (tileControl.tileStack.Peek().collisionTileStack.Contains(tile))
+                {
+                    tile.ChangeColor(); //색을 바꾼다.
+                }
+                else
+                {
+                    Debug.Log("멀리 떨어져있는 타일이여서 색 못바꿈");
                 }
             }
         }
-	}
+    }
 
 }
