ab84e618830f05f15d528c45fac54bd5710af9ce
HyeonMyeongJang <gkagm2@gmail.com>
2019-06-27 01:08:37 +0900

    주석 품

--- 20190502_FillOneLinePuzzle/Scripts/UnityAdsHelper.cs
+++ 20190502_FillOneLinePuzzle/Scripts/UnityAdsHelper.cs
@@ -3,21 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Advertisements;
 public class UnityAdsHelper : MonoBehaviour {
-    const string androidGameID = "xxxxxxxx";
-    const string IosGameID = "xxxxxxxx";
+    const string androidGameID = "3152813";
+    const string IosGameID = "3152812";
 
     const string rewardedVideoID = "rewardedVideo";
-	// Use this for initialization
-	void Start () {
-        
-	}
+    // Use this for initialization
+    void Start () {
+    }
 	
 	// Update is called once per frame
 	void Update () {
         Advertisement.Show(); // 광고 호출.
-	}
+    }
     private void Initialize()
     {
+
     }
     public void ShowRewardedAd()
     {
@@ -25,6 +25,7 @@ public class UnityAdsHelper : MonoBehaviour {
         {
             var options = new ShowOptions { resultCallback = HandleShowResult };
             Advertisement.Show("rewardedVideo", options);
+            Debug.Log("보상을 받는다.");
         }
     }
 
@@ -41,7 +42,6 @@ public class UnityAdsHelper : MonoBehaviour {
             case ShowResult.Failed:
                 Debug.LogError("The ad failed to be shown");
                 break;
-
         }
     }
 }
