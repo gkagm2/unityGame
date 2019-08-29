f779943bcc1751ccb60df86d7813f9b93f97dd4a
SagacityJang <gkagm2@gmail.com>
2019-08-29 17:48:38 +0900

    asset bundle 업데이트

---
+++ 20190829_AssetBundle/Assets/DownloadFromServer.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.Networking;
+public class DownloadFromServer : MonoBehaviour
+{
+    private const string downloadURL = "https://downloadj.run.goorm.io";
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(DownloadAssets());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    IEnumerator DownloadAssets()
+    {
+        UnityWebRequest www = UnityWebRequest.Get(downloadURL);
+        yield return www.SendWebRequest();
+
+        if(www.isNetworkError || www.isHttpError)
+        {
+            Debug.Log(www.error);
+        }
+        else
+        {
+            File.WriteAllBytes(Path.Combine(Application.streamingAssetsPath, "myasset"), www.downloadHandler.data);
+            Debug.Log("파일이 Assets/StreamingAssets에 저장되었습니다.");
+        }
+
+        /*
+        WWW www = new WWW(downloadURL);
+
+        yield return www;
+
+        if (www.isDone)
+        {
+            Debug.Log(www.text);
+            File.WriteAllBytes(Path.Combine(Application.streamingAssetsPath, "myasset"), www.bytes);
+            Debug.Log("파일이 Assets/StreamingAssets에 저장되었습니다.");
+        }
+        */
+    }
+}
---
+++ 20190829_AssetBundle/Assets/LoadFromFileExample.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+public class LoadFromFileExample : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(StartAssetBundle());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    IEnumerator StartAssetBundle()
+    {
+        var fileStream = new FileStream(Path.Combine(Application.streamingAssetsPath, "myasset"), FileMode.Open, FileAccess.Read);
+        var bundleLoadRequest = AssetBundle.LoadFromStreamAsync(fileStream);
+        yield return bundleLoadRequest;
+
+        var myLoadedAssetBundle = bundleLoadRequest.assetBundle;
+        if(myLoadedAssetBundle == null)
+        {
+            Debug.Log("Failed to load AssetBundle!");
+            yield break;
+        }
+        var assetLoadRequest = myLoadedAssetBundle.LoadAssetAsync<GameObject>("Y");
+        yield return assetLoadRequest;
+
+        GameObject prefab = assetLoadRequest.asset as GameObject;
+        Instantiate(prefab);
+
+        myLoadedAssetBundle.Unload(false);
+    }
+}
